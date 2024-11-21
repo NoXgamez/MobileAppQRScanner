81d725f0164eea925339f064ea97ee6a1524f80b
NoXgamez <s00236650@atu.ie>
2024-11-21 18:29:57 +0000

    completed lab

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("com.google.zxing:core:3.2.0")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 }
--- app/src/main/java/com/example/qrcode/MainActivity.java
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -1,13 +1,78 @@
 package com.example.qrcode;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+
+    //View Objects
+    public Button button;
+    public EditText etTitle, etAddress;
+
+    private IntentIntegrator qrScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.buttonScan);
+        etTitle = findViewById(R.id.etTitle);
+        etAddress = findViewById(R.id.etAddress);
+
+        qrScan = new IntentIntegrator(this);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                try {
+                    //converting the data to json
+                    JSONObject obj = new JSONObject(result.getContents());
+                    //setting values to textviews
+                    etTitle.setText(obj.getString("title"), TextView.BufferType.EDITABLE);
+                    etAddress.setText(obj.getString("website"), TextView.BufferType.EDITABLE);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    //if control comes here
+                    //that means the encoded format not matches
+                    //in this case you can display whatever data is available on the qrcode
+                    //to a toast
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public void OnClickScan(View view) {
+        qrScan.initiateScan();
+    }
+
+    public  void OnClickAddress(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(etAddress.getText().toString()));
+        startActivity(browserIntent);
     }
 }
